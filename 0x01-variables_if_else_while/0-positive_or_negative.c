a25f097aa8b3066d3d6d1b227172ff78419efdc9
Workshop8321 <Udaknse111@gmail.com>
2022-10-16 09:22:13 -0700

    The variable n will store a different value every time you will run this program

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - The variable n will store a different value 
+ * every time you will run this program.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+
+
+	int n;
+	
+
+	srand(time(0));
+	n = rand() - RAND_MAX /2;
+	/* your code goes there */
+
+	if ( n > 0)
+		{
+			printf(" %s is %d \n","postive", n);
+		}
+	else if  (n > 0)
+	{
+		printf("%s is %d \n", "zero", n);
+	}
+	else
+	{
+		printf("%s is %d \n", "negative", n);
+	}
+}
+
+
