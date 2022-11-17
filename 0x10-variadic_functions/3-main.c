d4755f445f3a6b77961eb838f1e1ac0992c0066e
Workshop8321 <Udaknse111@gmail.com>
2022-11-17 11:07:35 -0800

    3-print_all.c

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,14 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_all(NULL);
+    return (0);
+}
+Footer
+
